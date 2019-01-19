cade9101579ada809c7e092221cb5a745938170f
Saveliy Tronza <sparker_95@mail.ru>
2019-01-19 22:25:27 +0300

    NaturalDocs update

---
+++ HTML/files/Location/getRandomPos-sqf-Summary.js
@@ -0,0 +1,1 @@
+NDFramePage.OnPageTitleLoaded("File:Location/getRandomPos.sqf","getRandomPos.sqf");NDSummary.OnSummaryLoaded("File:Location/getRandomPos.sqf",[["SQF","SQF"]],[["Classes","Class"],["Functions","Function"],["Groups","Group"]],[[326,0,0,"Location","Location"],[375,0,2,"Functions","Location.Functions"],[376,0,1,"getRandomPos","Location.getRandomPos"]]);
