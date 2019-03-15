b48d92f6d4cdf59638aa04940c56278e436e3125
Jeroen-Notenbomer <j.notenbomer@live.nl>
2019-03-15 19:51:50 +0100

    disable default refuel

---
+++ @Project_0/addons/fuel/CfgVehicles.hpp
@@ -0,0 +1,75 @@
+class CfgVehicles {
+	class B_APC_Tracked_01_base_F;
+	class B_APC_Tracked_01_CRV_F: B_APC_Tracked_01_base_F
+	{
+		transportFuel = 0;
+	};
+	class B_Truck_01_mover_F;
+	class B_Truck_01_fuel_F: B_Truck_01_mover_F
+	{
+		transportFuel = 0;
+	};
+	class Truck_02_base_F;
+	class Truck_02_fuel_base_F: Truck_02_base_F
+	{
+		transportFuel = 0;
+	};
+	class Van_01_fuel_base_F;
+	class C_Van_01_fuel_F: Van_01_fuel_base_F
+	{
+		transportFuel = 0;
+	};
+	class I_G_Van_01_fuel_F: Van_01_fuel_base_F
+	{
+		transportFuel = 0;
+	};
+	class Truck_03_base_F;
+	class O_Truck_03_fuel_F: Truck_03_base_F
+	{
+		transportFuel = 0;
+	};
+	class StorageBladder_base_F;
+	class Land_StorageBladder_01_F: StorageBladder_base_F
+	{
+		transportFuel = 0;
+	};
+	class FlexibleTank_base_F;
+	class Land_FlexibleTank_01_F: FlexibleTank_base_F
+	{
+		transportFuel = 0;
+	};
+	class Slingload_01_Base_F;
+	class B_Slingload_01_Fuel_F: Slingload_01_Base_F
+	{
+		transportFuel = 0;
+	};
+	class Heli_Transport_04_base_F;
+	class O_Heli_Transport_04_fuel_F: Heli_Transport_04_base_F 
+	{
+		transportFuel = 0;
+	};
+	class Pod_Heli_Transport_04_base_F;
+	class Land_Pod_Heli_Transport_04_fuel_F: Pod_Heli_Transport_04_base_F 
+	{
+		transportFuel = 0;
+	};
+	class House_F;
+	class Land_FuelStation_01_pump_F: House_F
+	{
+		transportFuel = 0;
+	};
+	class Land_FuelStation_01_pump_malevil_F: House_F
+	{
+		transportFuel = 0;
+	};
+	class House_Small_F;
+	class Land_FuelStation_Feed_F: House_Small_F
+	{
+		transportFuel = 0;
+	};
+	class Land_fs_feed_F: House_Small_F
+	{
+		transportFuel = 0;
+	};
+	
+};
