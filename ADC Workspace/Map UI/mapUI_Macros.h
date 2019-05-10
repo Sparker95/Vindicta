a804344dc6e77e49ac1e2fc49b767bc1b275ff2b
marvis999 <44984956+marvis999@users.noreply.github.com>
2019-05-10 17:40:32 +0200

    macros comments added

--- ADC Workspace/Map UI/mapUI_Macros.h
+++ ADC Workspace/Map UI/mapUI_Macros.h
@@ -1,14 +1,13 @@
 //400
-#define IDC_HEADLINE 4001
-#define IDC_TABCAT 4002
-#define IDC_TAB1 4003
-#define IDC_TAB2 4004
-#define IDC_TAB3 4004
-#define IDC_DATA 4005
-#define IDC_BUTTON2 6002
-#define IDC_BUTTON1 6001
-#define IDC_BUTTON3 6003
-#define IDC_HINTPANEL 6004
-#define IDC_DECO1 6005
-#define IDC_BUTTONPANEL 6006
-
+#define IDC_HEADLINE 4001		// "Camp Foxtrot" headline
+#define IDC_TABCAT 4002			// Category/tab headline
+#define IDC_TAB1 4003			// first tab
+#define IDC_TAB2 4004			// second tab
+#define IDC_TAB3 4004			// third tab
+#define IDC_DATA 4005			// listbox data panel
+#define IDC_BUTTON2 6002		// bottom panel, button 2
+#define IDC_BUTTON1 6001		// bottom panel, button 1
+#define IDC_BUTTON3 6003		// bottom panel, button 3
+#define IDC_HINTPANEL 6004		// bottom panel, panel with hints
+#define IDC_DECO1 6005			// bottom panel, decorative line, cosmetic only
+#define IDC_BUTTONPANEL 6006	// bottom panel, background for buttons, decoration
