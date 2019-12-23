9b12e13090910196cc74e0637662a2952fcdf831
Saveliy Tronza <sparker_95@mail.ru>
2019-12-23 04:13:55 +0300

    Update AICommander.hpp

--- Project_0.Altis/AI/Commander/AICommander.hpp
+++ Project_0.Altis/AI/Commander/AICommander.hpp
@@ -142,7 +142,7 @@
 // Frequency of reinforcements, etc
 
 // How ofter commander will consider to import external reinforcements
-#define CMDR_EXT_REINF_INTERVAL_MINUTES 3
+#define CMDR_EXT_REINF_INTERVAL_MINUTES 45
 
 // Max amount of simultaneous actions
 #define CMDR_MAX_TAKE_OUTPOST_ACTIONS 10
