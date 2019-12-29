de39f66835342fca977f26f52ad480697f06d594
marvis999 <44984956+marvis999@users.noreply.github.com>
2019-12-29 19:00:37 +0100

    notifications redesign

--- Vindicta.Altis/UI/Notification/Notification.h
+++ Vindicta.Altis/UI/Notification/Notification.h
@@ -1,4 +1,4 @@
-#define __WIDTH 0.5
+#include "..\Resources\UIProfileColors.h"
 
 class NOTIFICATION_GROUP : MUI_GROUP_ABS
 {
@@ -10,72 +10,57 @@ class NOTIFICATION_GROUP : MUI_GROUP_ABS
 	style = 0;
 	text = "";
 	colorText[] = {0.5412,0.5255,0.051,1};
-	font = "PuristaMedium";
 	sizeEx = (((((safezoneW / safezoneH) min 1.2) / 1.2) / 25) * 1);
 
 	class Controls
 	{
 
-		class NOTIFICATION_BACKGROUND : MUI_BG_BLACKTRANSPARENT 
-		{
-			type = 0;
-			idc = -1;
-			x = 0;
-			y = 0;
-			w = __WIDTH;
-			h = 0.17;
-			style = 0;
-			text = "";
-			colorText[] = {0.5412,0.5255,0.051,1};
-			font = "PuristaMedium";
-			sizeEx = (((((safezoneW / safezoneH) min 1.2) / 1.2) / 25) * 1);
-			
-		};
-
 		class NOTIFICATION_ICON
 		{
 			type = 0;
 			idc = -1;
-			x = 0.0000002;
-			y = 0.0000003;
-			w = 0.05;
-			h = 0.05;
-			style = 0+48;
+			x = 0; 
+			y = 0; 
+			w = 0.026; 
+			h = 0.035;
+			style = ST_PICTURE + ST_KEEP_ASPECT_RATIO;
 			text = "\A3\ui_f\data\GUI\Rsc\RscDisplayMain\notification_ca.paa";
 			colorBackground[] = {0, 0, 0, 0};
 			colorText[] = {1, 1, 1, 1};
-			font = "PuristaMedium";
 			sizeEx = (((((safezoneW / safezoneH) min 1.2) / 1.2) / 25) * 1);
 			onLoad = "uiNamespace setVariable ['vin_not_icon', _this#0]";
 		};
-		class NOTIFICATION_CATEGORY : MUI_BASE_ABS
+		class NOTIFICATION_CATEGORY : MUI_HEADLINE_ABS
 		{
 			type = 0;
 			idc = -1;
-			x = 0.05000032;
-			y = 0.00000031;
-			w = __WIDTH - 0.05;
-			h = 0.05;
+			x = 0.029; 
+			y = 0; 
+			w = 0.407; 
+			h = 0.035;  
+
+			colorBackground[] = MUIC_MISSION;
+
 			style = 0;
 			text = "INTEL";
-			colorBackground[] = {0.702,0.102,0.102,0.3572};
-			colorText[] = {1,1,1,1};
-			font = "PuristaMedium";
-			sizeEx = 0.05;
+			sizeEx = 0.035;
 			onLoad = "uiNamespace setVariable ['vin_not_category', _this#0]"; // Dont delete this!
 		};
 		class NOTIFICATION_TEXT : MUI_BASE_ABS
 		{
 			type = 0;
 			idc = -1;
-			x = 0.00000066;
-			y = 0.05000038;
-			w = __WIDTH;
-			h = 0.08;
+			x = 0; 
+			y = 0.041; 
+			w = 0.436; 
+			h = 0.081;  
+
+			colorBackground[] = {0, 0, 0, 0.65};
+
 			style = 0+16+512;
 			text = "Enemy radio cryptokey was found!";
-			font = "PuristaMedium";
-			sizeEx = 0.04;
+			font = "RobotoCondensed";
+			sizeEx = 0.035;
 			lineSpacing = 1;
 			onLoad = "uiNamespace setVariable ['vin_not_text', _this#0]";  // Dont delete this!
 		};
@@ -83,15 +68,18 @@ class NOTIFICATION_GROUP : MUI_GROUP_ABS
 		{
 			type = 0;
 			idc = -1;
-			x = 0.00000043;
-			y = 0.1300005;
-			w = __WIDTH;
-			h = 0.04000017;
+			x = 0; 
+			y = 0.123; 
+			w = 0.436; 
+			h = 0.035; 
+
+			colorBackground[] = {0, 0, 0, 1};
+
 			style = 0+2;
 			text = "Cryptokeys are stored in your notes tab";
 			colorText[] = {1,1,1,1};
-			font = "PuristaMedium";
-			sizeEx = 0.04;
+			font = "RobotoCondensed";
+			sizeEx = 0.035;
 			onLoad = "uiNamespace setVariable ['vin_not_hint', _this#0]";  // Dont delete this!
 		};
 		
