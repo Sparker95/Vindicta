504c52aef9e39edf724f1cb68e54e0214b18345e
marvis999 <44984956+marvis999@users.noreply.github.com>
2020-01-26 15:26:20 +0100

    added some BuildUI objects

--- Vindicta.Altis/UI/BuildUI/BuildUI_Objects.h
+++ Vindicta.Altis/UI/BuildUI/BuildUI_Objects.h
@@ -98,6 +98,9 @@ class BuildObjects
 			// Lights
 			__BUILD_OBJECT_CLASS(Light0,Land_TentLamp_01_standing_F, 10)
 			__BUILD_OBJECT_CLASS(Light1,Land_LampShabby_F, 20)
+			__BUILD_OBJECT_CLASS(Light2,Campfire_burning_F, 5)
+
+			
 		};
 
 		class Defense {
@@ -107,6 +110,16 @@ class BuildObjects
 			__BUILD_OBJECT_CLASS(Defense1,Land_HBarrier_3_F, 20)
 			__BUILD_OBJECT_CLASS(Defense2,Land_HBarrierWall6_F, 30)
 			__BUILD_OBJECT_CLASS(Defense3,Land_HBarrierTower_F, 40)
+
+			// cheap makeshift barriers
+			__BUILD_OBJECT_CLASS(Defense4,Land_SandbagBarricade_01_hole_F, 10)
+			__BUILD_OBJECT_CLASS(Defense5,Land_SandbagBarricade_01_F, 10)
+			__BUILD_OBJECT_CLASS(Defense6,Land_SandbagBarricade_01_half_F, 5)
+			__BUILD_OBJECT_CLASS(Defense7,Land_Barricade_01_10m_F, 15)
+
+			// dirt mound
+			__BUILD_OBJECT_CLASS(Defense8,Dirthump_1_F, 20)
+			
 		};
 
 		class Special {
