72eacab423b6cb9383b747093173fd50728f1945
billw2012 <billw2011@gmail.com>
2020-01-31 13:33:31 +0000

    Fix github validate action

--- devsetup.bat
+++ devsetup.bat
@@ -1,4 +1,5 @@
 @echo off
-cd /d "%~dp0"
+PUSHD "%~dp0"
 copy Vindicta.Altis\config\user_local_config.hpp.template Vindicta.Altis\config\user_local_config.hpp
 call tools\copy_sqm_from_github.bat
+POPD
