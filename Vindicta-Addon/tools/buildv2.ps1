1842211b2042f6f9f5d7e422299ea7ca86550e8b
billw2012 <billw2011@gmail.com>
2020-02-04 19:27:47 +0000

    Switch to Arma tools for signing

--- Vindicta-Addon/tools/buildv2.ps1
+++ Vindicta-Addon/tools/buildv2.ps1
@@ -5,19 +5,22 @@ $verStr = $verDir -replace "vindicta_v",""
 # (Get-Content -path hemtt.json -Raw) -replace '0.0.0',$verStr | Set-Content -Path hemtt.json
 # .\tools\hemtt.exe build --release --force --nowarn
 
-.\tools\armake_w64 keygen "vindicta_v$($verStr)"
-$privateKey = "vindicta_v$($verStr).biprivatekey"
+.\tools\DSCreateKey "vindicta"
+$privateKey = "vindicta.biprivatekey"
 New-Item ".\release\@vindicta\keys" -ItemType "directory"
 New-Item ".\release\@vindicta\addons" -ItemType "directory"
 Copy-Item "vindicta_v$($verStr).bikey" ".\release\@vindicta\keys\vindicta_v$($verStr).bikey"
-.\tools\armake_w64 build -i include -k $privateKey "..\_build\$($verDir)" ".\release\@vindicta\addons\vindicta_v$($verStr).pbo" -w unquoted-string -w redefinition-wo-undef -w excessive-concatenation
+.\tools\armake_w64 build -i include "..\_build\$($verDir)" ".\release\@vindicta\addons\vindicta_v$($verStr).pbo" -w unquoted-string -w redefinition-wo-undef -w excessive-concatenation
+.\tools\DSSignFile $privateKey ".\release\@vindicta\addons\vindicta_v$($verStr).pbo"
+
 # .\tools\armake_w64 sign "vindicta_v$($verStr).biprivatekey" ".\release\@vindicta\addons\vindicta_v$($verStr).pbo"
 
 $modules = Get-ChildItem -Path "addons" -Directory
 
 foreach ($module in $modules) {
     $pboName = ".\release\@vindicta\addons\vindicta_$($module.Name).pbo"
-    .\tools\armake_w64 build -i include -k $privateKey "addons\$($module.Name)" $pboName -w unquoted-string -w redefinition-wo-undef -w excessive-concatenation
+    .\tools\armake_w64 build -i include  "addons\$($module.Name)" $pboName -w unquoted-string -w redefinition-wo-undef -w excessive-concatenation
+    .\tools\DSSignFile $privateKey $pboName
     # .\tools\armake_w64 sign "vindicta_v$($verStr).biprivatekey" $pboName
 }
 
---
+++ Vindicta-Addon/tools/readme.txt
@@ -0,0 +1,12 @@
+PBO signing tool
+
+==================================
+
+	© 2013-2017 Bohemia Interactive a.s.
+
+	All rights reserved.
+
+	For more information see community.bistudio.com
+		Check sign	https://community.bistudio.com/wiki/DSCheckSignatures
+		Create key 	https://community.bistudio.com/wiki/DSCreateKey
+		Sign file 	https://community.bistudio.com/wiki/DSSignFile
