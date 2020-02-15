7e1ef0b18a752aea65570e515117929e953b03e7
billw2012 <billw2011@gmail.com>
2020-02-15 14:21:13 +0000

    Fix edit map script to abort if the sqm is locked

--- tools/edit_map.bat
+++ tools/edit_map.bat
@@ -1,8 +1,14 @@
 rem @echo off
 cd /d "%~dp0.."
-if exist editing.Altis (copy /Y Vindicta.Altis\mission.sqm mission.Altis.sqm & del editing.Altis)
-if exist editing.Enoch (copy /Y Vindicta.Enoch\mission.sqm mission.Enoch.sqm & del editing.Enoch)
-if exist editing.Malden (copy /Y Vindicta.Malden\mission.sqm mission.Malden.sqm & del editing.Malden)
+
+ren Vindicta.Altis\mission.sqm mission.sqm.tmp
+if errorlevel 1 (
+    echo mission.sqm file is locked, close the editor before running this
+    exit /b 0
+)
+if exist editing.Altis (copy /Y Vindicta.Altis\mission.sqm.tmp mission.Altis.sqm & del editing.Altis)
+if exist editing.Enoch (copy /Y Vindicta.Altis\mission.sqm.tmp mission.Enoch.sqm & del editing.Enoch)
+if exist editing.Malden (copy /Y Vindicta.Altis\mission.sqm.tmp mission.Malden.sqm & del editing.Malden)
 
 copy /Y mission.%1.sqm Vindicta.%1\mission.sqm
 echo editing.%1 >editing.%1
