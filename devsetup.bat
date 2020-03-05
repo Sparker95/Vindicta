164bd2e5c5a81d3f033b2f297c51b8feef2ba32a
Panda <61051804+rc95@users.noreply.github.com>
2020-03-05 04:55:17 +0000

    Added vt7

--- devsetup.bat
+++ devsetup.bat
@@ -4,5 +4,6 @@ if not exist Vindicta.Altis\config\user_local_config.hpp copy Vindicta.Altis\con
 if not exist Vindicta.Enoch mklink /D /J Vindicta.Enoch Vindicta.Altis
 if not exist Vindicta.Malden mklink /D /J Vindicta.Malden Vindicta.Altis
 if not exist Vindicta.Staszow mklink /D /J Vindicta.Staszow Vindicta.Altis
+if not exist Vindicta.vt7 mklink /D /J Vindicta.vt7 Vindicta.Altis
 call edit_altis.bat
 POPD
--- tools/edit_map.bat
+++ tools/edit_map.bat
@@ -13,6 +13,7 @@ if exist editing.Altis (copy /Y Vindicta.Altis\mission.sqm mission.Altis.sqm & d
 if exist editing.Enoch (copy /Y Vindicta.Altis\mission.sqm mission.Enoch.sqm & del editing.Enoch)
 if exist editing.Malden (copy /Y Vindicta.Altis\mission.sqm mission.Malden.sqm & del editing.Malden)
 if exist editing.Staszow (copy /Y Vindicta.Altis\mission.sqm mission.Staszow.sqm & del editing.Staszow)
+if exist editing.vt7 (copy /Y Vindicta.Altis\mission.sqm mission.vt7.sqm & del editing.vt7)
 goto :edit
 
 :clean
@@ -20,6 +21,7 @@ if exist editing.Altis del editing.Altis
 if exist editing.Enoch del editing.Enoch
 if exist editing.Malden del editing.Malden
 if exist editing.Staszow del editing.Staszow
+if exist editing.vt7 del editing.vt7
 
 :edit
 copy /Y mission.%1.sqm Vindicta.%1\mission.sqm
